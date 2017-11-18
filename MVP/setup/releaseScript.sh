c42859a2a33d488817471fd5e59cdb0085f166a4
webbhm <webb.howard@gmail.com>
2017-11-18 16:10:57 -0500

    Update releaseScript.sh

--- MVP/setup/releaseScript.sh
+++ MVP/setup/releaseScript.sh
@@ -41,7 +41,7 @@ echo $(date -u) "directories created"
 
 # Install CouchDB
 
-chown +x $TARGET/setup/couch.sh
+sudo chown +x $TARGET/setup/couch.sh
 $TARGET/setup/couch.sh || error_exit "Failure to install CouchDB"
 
 # Install Libraries
