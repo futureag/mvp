eb41fe685fe8589670ce405e4783673c49b101dd
webbhm <webb.howard@gmail.com>
2017-11-18 15:34:31 -0500

    Rename logSensors.py to logSensors-Hold.py

--- MVP/python/logSensors-Hold.py
+++ MVP/python/logSensors-Hold.py
@@ -1,7 +1,6 @@
 #Check sensors and log to file
 from si7021 import getTempC, getHumidity
 from logData import logData
-#from ds18B29 import getTempC as getTempC_ds
 
 try:
     temp = getTempC()
@@ -14,10 +13,3 @@ try:
     logData("si7021_top", "Success", "humidity", "{:10.1f}".format(humid), '')
 except (IOError, e):
         logData("si7921_top", "Failure", "humidity", '', str(e))
-
-#try:
-#    tempDS = getTempC_ds()
-#    logData("DS18B29_temp", "Success", "temperature", "{:.1f}".format(tempDS), '')
-#except (IndexError):    
-#    logData("DS18B29_temp", "Failure", "temperature", '', "Data file not found or not read")
- 
