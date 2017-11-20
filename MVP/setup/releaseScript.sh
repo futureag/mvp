616dd562a0bbd1cd18c8ce71f7082c54620c9142
webbhm <webb.howard@gmail.com>
2017-11-20 09:09:01 -0500

    Update releaseScript.sh

--- MVP/setup/releaseScript.sh
+++ MVP/setup/releaseScript.sh
@@ -104,6 +104,32 @@ python $PYTHON/testScript.py || error_exit "Failure of test script"
 sudo bash /home/pi/MVP/scripts/render.sh
 echo $(date +"%D %T") "System PASSED"
 
+# Change CouchDB for access on network
+# modify /home/couchdb/etc/local.ini
+COUCH=/home/couchdb/etc/local.ini
+COUCH2=/home/pi/scripts/test2.txt
+if grep -q "bind_address = 0.0.0.0" $COUCH;
+then
+	echo $COUCH" already has bind address"
+else
+	sed -e 's/;bind_address = 127.0.0.1/bind_address = 0.0.0.0/' $COUCH > $COUCH2
+	mv -f $COUCH2 $COUCH
+	echo "bind address added to "$COUCH
+fi
+
+# Set startup script
+# Modify /etc/rc.local
+RC_LOCAL=/etc/rc.local
+RC_LOCAL2=/etc/rc.local2
+if grep -q "startup.sh" /home/pi/scripts/test.txt;
+then
+	echo $RC_LOCAL" already has startup command"
+else
+	sed -e 's/exit 0/\/home\/pi\/MVP\/scripts\/startup.sh'\\n\\'nexit 0/' $RC_LOCAL > $RC_LOCAL2
+	mv -f $RC_LOCAL2 $RC_LOCAL
+	echo "startup.sh added to "$RC_LOCAL
+fi
+
 # Load Cron
 crontab /home/pi/scripts/MVP_cron.txt
 
