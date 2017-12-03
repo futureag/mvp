d27f147de8ab3353fbf536363e816f99bf7e37e2
webbhm <webb.howard@gmail.com>
2017-12-03 09:25:07 -1000

    Add files via upload

--- MVP/scripts/cron.sh
+++ MVP/scripts/cron.sh
@@ -1,7 +1,31 @@
+#!/bin/sh
+
+# Load configuration to cron
+# Author: Howard Webb
+# Date: 11/16/2017
+
+#######################################
+
+TARGET=/home/pi/MVP
+PYTHON=$TARGET/python
+
+# Declarations
+RED='\033[31;47m'
+NC='\033[0m'
+
+# Exit on error
+error_exit()
+{
+	echo ${RED} $(date +"%D %T") "${PROGNAME}: ${1:="Unknown Error"}" ${NC} 1>&2
+	exit 1
+}
+
+##########################################
+
+
 # MVP script to load cron job file
-# send output to a log file
 
 timestamp="$(date +"%D %T")"
 
-#echo $timestamp && crontab /home/pi/scripts/MVP_cron.txt >> /home/pi/MVP/logs/sys.log 2>$1
-crontab /home/pi/scripts/MVP_cron.txt
+crontab /home/pi/MVP/scripts/MVP_cron.txt || error_exit "Failure loading Cron"
+echo $(date +"%D %T") "Cron loaded"
