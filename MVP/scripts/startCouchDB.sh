32db9725a00ed493294e23769804252ba108e5c1
webbhm <webb.howard@gmail.com>
2017-12-20 09:24:15 -0500

    Update startCouchDB.sh

--- MVP/scripts/startCouchDB.sh
+++ MVP/scripts/startCouchDB.sh
@@ -4,4 +4,10 @@
 #Author: Howard Webb
 #Date: 7/15/2017
 
-nohup sudo -i -u couchdb /home/couchdb/bin/couchdb &>~/MVP/logs/couchdb.log &
+sudo touch /home/pi/MVP/logs/couchdb.log
+sudo chmod a+r /home/pi/MVP/logs/couchdb.log
+sudo chmod a+w /home/pi/MVP/logs/couchdb.log
+sudo chmod a+x /home/pi/MVP/logs/couchdb.log
+
+nohup sudo -i -u couchdb /home/couchdb/bin/couchdb &/home/pi/MVP/logs/couchdb.log &
+
