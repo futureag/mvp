0701740c4d5bea526cf67d505b64207f3d129488
webbhm <webb.howard@gmail.com>
2018-03-08 05:29:44 -1000

    Add files via upload
    added logging for confirmation

--- MVP/scripts/startup.sh
+++ MVP/scripts/startup.sh
@@ -7,8 +7,12 @@
 #Author: Howard Webb
 #Date: 7/15/2017
 
-# Start the server
-/home/pi/MVP/scripts/startServer.sh
+echo Running Startup
 
+echo Starting Server
+# Start the server
+/home/pi/MVP/scripts/startServer.sh >> /home/pi/MVP/logs/startup.log 2>&1
+
+echo Starting CouchDB
 # Start CouchDB
-/home/pi/MVP/scripts/startCouchDB.sh
+/home/pi/MVP/scripts/startCouchDB.sh >> /home/pi/MVP/logs/startup.log 2>&1
