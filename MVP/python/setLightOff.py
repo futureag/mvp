68c70b73d67cfbd2d00907c211f6f9260211dba0
webbhm <webb.howard@gmail.com>
2018-04-06 17:18:34 -0400

    Update setLightOff.py
    revert to old compatible version

--- MVP/python/setLightOff.py
+++ MVP/python/setLightOff.py
@@ -1,42 +1,21 @@
+#!/usr/bin/python
+
 #Light Control
 #Controls the turning on and turning off of lights
 #Lights are wired into Relay #4 (Pin 29)
-# Author: Howard Webb
-# Date: 3/5/2018
 
 import RPi.GPIO as GPIO
-from JsonUtil import makeEnvJson
-import CouchDB
+from logData import logData
 
 
-
-def setLightOff(test=False):
+def setLightOff():
     "Check the time and determine if the lights need to be changed"
     lightPin = 29
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     print ("Turn lights Off")
     GPIO.setup(lightPin, GPIO.OUT)
-# For the relaly board, use the first line
-# For the Sparkfun PowerSwitch tail (https://www.sparkfun.com/products/10747)
-# Uncomment the second line, and comment out the first
     GPIO.output(lightPin, GPIO.LOW)
-#    GPIO.output(lightPin, GPIO.HIGH)
-    logState("Off", test)
-
-def logState(value, test=False):
-    status_qualifier='Success'
-    if test:
-        status_qualifier='Test'
-    jsn=makeEnvJson('State_Change', 'Lights', 'Top', 'State', value, 'Lights', status_qualifier)
-    CouchDB.logEnvObsvJSON(jsn)
-
-def test():
-    print "Test Lights Off"
-    setLightOff(True)    
-
-if __name__=="__main__":
-    setLightOff()
-            
+    logData("Light_Switch", "Success", "light", "Off", '')
     
-
+setLightOff()  
