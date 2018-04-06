45bed31af2acc298c3d99410f091006d5a95a3f3
webbhm <webb.howard@gmail.com>
2018-04-06 17:19:35 -0400

    Update setLightOn.py
    revert to old compatible version

--- MVP/python/setLightOn.py
+++ MVP/python/setLightOn.py
@@ -1,39 +1,21 @@
+#!/usr/bin/python
+
 #Light Control
 #Controls the turning on and turning off of lights
 #Lights are wired into Relay #4 (Pin 29)
 
 import RPi.GPIO as GPIO
-from JsonUtil import makeEnvJson
-import CouchDB
+from logData import logData
 
-def setLightOn(test=False):
+
+def setLightOn():
     "Check the time and determine if the lights need to be changed"
     lightPin = 29
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BOARD)
     print ("Turn lights On")
     GPIO.setup(lightPin, GPIO.OUT)
-# For the relaly board, use the first line
-# For the Sparkfun PowerSwitch tail (https://www.sparkfun.com/products/10747)
-# Uncomment the second line, and comment out the first    
     GPIO.output(lightPin, GPIO.HIGH)
-#    GPIO.output(lightPin, GPIO.LOW)    
-    logState("Off", test)
+    logData("LightChange", "Success", "light", "On", '')
 
-def logState(value, test=False):
-    status_qualifier='Success'
-    if test:
-        status_qualifier='Test'
-    jsn=makeEnvJson('State_Change', 'Lights', 'Top', 'State', value, 'Lights', status_qualifier)
-    CouchDB.logEnvObsvJSON(jsn)
-
-def test():
-    print "Test Lights On"
-    setLightOn(True)
-
-if __name__=="__main__":
-    setLightOn()    
-    
-            
-    
-
+setLightOn()    
